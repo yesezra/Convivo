6a773d684c2f42e1842e98b16d141732e2d07eac
Ezra Spier <Ezra.Spier@gmail.com>
2010-06-18 12:39:30 -0700

    more testing, adding factories

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+Factory.define :farmer do |farmer|
+  farmer.name       "A Farmer"
+end
+
+Factory.define :washing_station do |washing_station|
+  washing_station.name      "Washing Station 1"
+  washing_station.code      "abcabc123"
+end
--- spec/models/farmer_spec.rb
+++ spec/models/farmer_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Farmer do
+  @washing_station = Factory(:washing_station)
   before(:each) do
     @valid_attributes = {
       :name => "Farmer Name"
@@ -15,4 +16,18 @@ describe Farmer do
     no_name_farmer = Farmer.new(@valid_attributes.merge(:name => ""))
     no_name_farmer.should_not be_valid
   end
+  
+  # describe "washing Station associations" do
+  #   before(:each) do
+  #     @farmer = @washing_station.farmer.create(@valid_attributes)
+  #   end
+  #   
+  #   it "should have a washing station attribute" do
+  #     @farmer.should respond_to(:washing_station)
+  #   end
+  #   
+  #   it "should have the right associated washing station" do
+  #     @farmer.washing_station.should == @washing_station
+  #   end
+  # end
 end
